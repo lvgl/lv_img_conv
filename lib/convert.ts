35116eb939b644a6f8cc044de929c78149a4a60a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-11-28 08:20:22 -0500

    Change include strategy for lvgl.h

--- lib/convert.ts
+++ lib/convert.ts
@@ -211,10 +211,18 @@ class Converter {
 
     get_c_header(out_name: string): string {
         var $c_header =
-        `#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
-#include "lvgl.h"
+        `#ifdef __has_include
+    #if __has_include("lvgl.h")
+        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
+            #define LV_LVGL_H_INCLUDE_SIMPLE
+        #endif
+    #endif
+#endif
+
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
+    #include "lvgl.h"
 #else
-#include "lvgl/lvgl.h"
+    #include "lvgl/lvgl.h"
 #endif
 
 
